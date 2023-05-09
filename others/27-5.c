ffbcdeec9fbc9f2c751b79e9e5e8444b2c6400b0
dipakdhoke <kuldipakdhoke@gmail.com>
2023-05-09 10:17:51 +0530

    practice

---
+++ others/27-5.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+ int i ,z,x;
+for (i=5;i>=1;i--){
+
+      for (z=5;z>=i;z--){
+	printf(" ");
+     }
+      for (x=1;x<=i;x++){
+      printf("%d",x);
+      }
+     printf("\n");
+
+
+     }
+     return 0;
+
+
+      }
+
+
+
+
